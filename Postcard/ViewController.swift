eb49662fd4cdb4f8f753291aecffada0dd2a485f
Reynhart de Beer <ReyMac@Reynharts-MacBook-Pro.local>
2014-09-18 16:07:32 +0200

    added new label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -38,7 +40,12 @@ class ViewController: UIViewController {
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
-        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.text = ""
+        nameLabel.textColor = UIColor.blueColor()
+
+
     }
 
 }
